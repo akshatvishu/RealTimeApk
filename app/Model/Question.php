0b6aca6d445884decdadc6024418176e644cf180
Akshat Nayak <akshatnayak197@gmai.com>
2019-06-16 12:55:06 +0530

    CURD for Quenstion with Question Resource and transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,7 +6,14 @@ use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
+
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -19,5 +26,9 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
